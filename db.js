247e24ba2bdae3ccc4a7f540101415f1b6e4e9e3
Abdul Mboob <67025021+amboob223@users.noreply.github.com>
2023-02-27 22:29:50 -0500

    we added env varibles

--- db.js
+++ db.js
@@ -1,11 +1,6 @@
 const POOL = require("pg").Pool
-
 const pool = new POOL({
-    user:"postgres",
-    host:"localhost",
-    port:5432,
-    password:"8896",
-    database:"gsite"
-});
+    connectionString:process.env.DATABASE_URL
+})
 
 module.exports = pool;
